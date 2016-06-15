b5f601ad632a6f8af8c5d63df40a9ae3cbcbc394
Supriya2807 <lahadesupriya@gmail.com>
2016-06-15 16:29:39 +0530

    session4assign1
    reverse of an array

---
+++ session4assign1.java
@@ -0,0 +1,29 @@
+package acadgild.assignments;
+
+public class session4assign1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Print reverse of an Array..
+		
+		int num[] = {12,56,23,16,45,78,1,34,10,91};
+		
+		
+		for(int i=(num.length-1);i>=0;i--){
+			
+			
+		
+			
+			System.out.print("\t" +num[i]);
+		
+
+			
+		}
+		
+		
+		
+
+	}
+
+}
